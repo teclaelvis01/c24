b56adc4f04c473541e094a063fb0e227a055da77
Elvis Reyes <teclaelvis01@gmail.com>
2025-05-05 11:40:20 +0000

    feat: :sparkles: add migration to update logoUrl and deepLink fields to nullable in credit_card_manual_edits table

---
+++ migrations/Version20250505113947.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250505113947 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE credit_card_manual_edits CHANGE logo_url logo_url VARCHAR(255) DEFAULT NULL, CHANGE deep_link deep_link VARCHAR(500) DEFAULT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE credit_card_manual_edits CHANGE logo_url logo_url VARCHAR(255) NOT NULL, CHANGE deep_link deep_link VARCHAR(500) NOT NULL
+        SQL);
+    }
+}
